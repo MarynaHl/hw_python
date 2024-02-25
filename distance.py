eae2b1409996d2a1fc9ce854f414d0eb287f5c5b
hlushak <75501308+MarynaHl@users.noreply.github.com>
2024-02-25 13:26:53 +0000

    Task 4 upd

---
+++ distance.py
@@ -0,0 +1,5 @@
+def meters_to_feet(meters):
+    return meters * 3.28084
+
+def feet_to_meters(feet):
+    return feet / 3.28084
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
-from temperature import celsius_to_fahrenheit, fahrenheit_to_celsius, meters_to_feet, feet_to_meters
+from temperature import celsius_to_fahrenheit, fahrenheit_to_celsius
+from distance import meters_to_feet, feet_to_meters
 
 def convert_temperature_and_distance(input_file, output_file, target_unit):
     with open(input_file, 'r') as csvfile:
