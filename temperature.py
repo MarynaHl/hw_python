b4ee2bc02da7fc48a5937022880f8e041646b6e4
hlushak <75501308+MarynaHl@users.noreply.github.com>
2024-02-25 11:11:57 +0000

    Task 2

--- temperature.py
+++ temperature.py
@@ -1,10 +1,15 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
 def convert_temperature(temperature_str):
     if '°C' in temperature_str:
         temperature = int(temperature_str.split('°C')[0])
-        return (temperature * 9/5) + 32
+        return celsius_to_fahrenheit(temperature)
     elif '°F' in temperature_str:
         temperature = int(temperature_str.split('°F')[0])
-        return (temperature - 32) * 5/9
+        return fahrenheit_to_celsius(temperature)
     else:
         raise ValueError('Invalid temperature format')
-
